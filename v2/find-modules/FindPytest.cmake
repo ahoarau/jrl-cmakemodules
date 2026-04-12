0909b80fa36bee191c2531714f02c3858d4c9c6a
Antoine Hoarau <703240+ahoarau@users.noreply.github.com>
2026-04-12 23:50:55 +0200

    find-modules: add QUIET support

--- v2/find-modules/FindPytest.cmake
+++ v2/find-modules/FindPytest.cmake
@@ -17,19 +17,24 @@ endif()
 cmake_path(GET Python_EXECUTABLE PARENT_PATH Python_ROOT)
 
 # On Ubuntu 22.04, pytest is named pytest-3
-find_program(Pytest_EXECUTABLE NAMES pytest pytest-3 HINTS ${Python_ROOT}/Scripts REQUIRED)
+find_program(Pytest_EXECUTABLE NAMES pytest pytest-3 HINTS ${Python_ROOT}/Scripts)
 
 # On Ubuntu 22.04, pytest prints the version in stderr
-execute_process(
-    COMMAND ${Pytest_EXECUTABLE} --version
-    OUTPUT_VARIABLE Pytest_VERSION_FULL
-    ERROR_VARIABLE Pytest_VERSION_FULL
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-string(REGEX MATCH "[0-9]+\\.[0-9]+(\\.[0-9a-zA-Z]+)?" Pytest_VERSION "${Pytest_VERSION_FULL}")
+if(Pytest_EXECUTABLE)
+    execute_process(
+        COMMAND ${Pytest_EXECUTABLE} --version
+        OUTPUT_VARIABLE Pytest_VERSION_FULL
+        ERROR_VARIABLE Pytest_VERSION_FULL
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    string(REGEX MATCH "[0-9]+\\.[0-9]+(\\.[0-9a-zA-Z]+)?" Pytest_VERSION "${Pytest_VERSION_FULL}")
 
-if(NOT Pytest_VERSION)
-    message(FATAL_ERROR "Could not determine Pytest version from output: '${Pytest_VERSION_FULL}'")
+    if(NOT Pytest_VERSION)
+        message(
+            FATAL_ERROR
+            "Could not determine Pytest version from output: '${Pytest_VERSION_FULL}'"
+        )
+    endif()
 endif()
 
 mark_as_advanced(Pytest_EXECUTABLE Pytest_VERSION)
@@ -37,7 +42,7 @@ mark_as_advanced(Pytest_EXECUTABLE Pytest_VERSION)
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Pytest REQUIRED_VARS Pytest_EXECUTABLE VERSION_VAR Pytest_VERSION)
 
-if(NOT TARGET Pytest::Pytest)
+if(Pytest_FOUND AND NOT TARGET Pytest::Pytest)
     add_executable(Pytest::Pytest IMPORTED)
     set_target_properties(
         Pytest::Pytest
